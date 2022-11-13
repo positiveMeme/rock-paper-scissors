030e491f68ba6f2043b00eb6c0efe399d4fdf123
positiveMeme <joe.arthurs@hotmail.co.uk>
2022-11-13 14:42:30 -0500

    Add index.html and script.js to main. All working well

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rock-paper-scissors
 First application of Javascript
+
+This is my first attempt at using Javascript on a webpage, following the Odin Project course.
---
+++ script.js
@@ -0,0 +1,62 @@
+const choices = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+    results();
+}
+
+function playRound() {
+    const playerSelection = playerChoice();
+    const computerSelection = compChoice();
+    const winner = checkWinner(playerSelection, computerSelection);
+    console.log("player: " + playerSelection);
+    console.log("computer: " + computerSelection);
+    console.log(winner);
+}
+
+function playerChoice() {
+    let input = prompt("Choose either rock, paper or scissors");
+    input = input.toLowerCase();
+    return input;
+}
+
+function compChoice() {
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function checkWinner(choiceP, choiceC) {
+    if (
+        (choiceP === "scissors" && choiceC === "paper") ||
+        (choiceP === "rock" && choiceC === "scissors") ||
+        (choiceP === "paper" && choiceC === "rock")) {
+        playerScore++;
+        return `You win, ${choiceP} beats ${choiceC}`;
+    }
+    else if (choiceP === choiceC) {
+        return "It's a tie!";
+    }
+    else {
+        computerScore++;
+        return `You lose, ${choiceC} beats ${choiceP}`;
+    }
+}
+
+function results() {
+    console.log(`Player score: ${playerScore}`);
+    console.log(`Computer score: ${computerScore}`);
+    if (playerScore > computerScore) {
+        console.log("You win!");
+    }
+    else if (playerScore < computerScore) {
+        console.log("You lose!");
+    }
+    else {
+        console.log("It's a draw!");
+    }
+}
+
+game();
